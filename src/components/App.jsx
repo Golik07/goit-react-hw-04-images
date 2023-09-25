d56f6fda5bc88630ac856acd32362817644abd5f
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-09-25 18:29:42 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,67 @@
-import {useState} from "react";
+import {useState,useEffect} from "react"
 import Searchbar from './Searchbar'
 import css from "./app.module.css"
-
+import Button from "./Button/Button"
+import {api}  from './service/api';
 import ImageGallery from "./ImageGallery";
+import { Audio } from 'react-loader-spinner'
 
 export const App = () => {
 
-const [name,setName] = useState('');
+  const [cards,setCards] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+  const [button,setButton] = useState(false);
+  const [noData,setNoData] = useState(false);
+  const [page,setPage] = useState(1);
+  const [name,setName] = useState('');
 
 const addValue = value => {
+  if(value !== name){
+    setCards([])
     setName(value);
+    setIsLoading(true)
+    setButton(false)
+    setPage(1);
+  }
   };
+
+  useEffect(()=> {
+    name &&
+     api(name,page).then(({total,hits} ) => {
+         if(total === 0){
+             setCards([])
+             setIsLoading(false)
+             setNoData(true)
+             setButton(false)
+             return;
+         }
+         setIsLoading(true)
+         setCards(prev => [...prev,...hits])
+           setIsLoading(false)
+           setNoData(false)
+           setButton(true)
+     }).catch((error) => console.log(error))
+ },[name,page])
+
+  const onClick = () => {
+    setPage(prev => prev + 1)
+ }
 
     return(
       <div className={css.App}>
         <Searchbar onSubmit={addValue}/>
-        <ImageGallery  value={name} />
+        {isLoading && <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="loading"
+            wrapperStyle
+            wrapperClass
+          />}
+        {noData && (<h1>No data found</h1>)}
+        <ImageGallery values={cards} />
+        {button && <Button click={onClick}/>}
       </div>
     )
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,77 +1,19 @@
 import ImageGalleryItem from "../ImageGalleryItem"
 import css from "./ImageGallery.module.css"
-import {useState,useEffect} from "react"
-import {api}  from '../service/api';
-import Button from "../Button/Button"
-import { Audio } from 'react-loader-spinner'
 
-const ImageGallery = ({value}) => {
-
-    const [cards,setCards] = useState([]);
-    const [isLoading,setIsLoading] = useState(false);
-    const [button,setButton] = useState(false);
-    const [noData,setNoData] = useState(false);
-    const [page,setPage] = useState(1);
-
-
-    const onClick = () => {
-        
-        const nextPage = page + 1;
-
-        api(value,nextPage).then(({hits} ) => {
-            setCards(prev => [...prev,...hits])
-            setIsLoading(false)
-            setPage(nextPage)
-            setNoData(false)
-            setButton(true)
-        }).catch((error) => console.log(error))
-    }
-
-
-    useEffect(()=> {
-       value &&
-       
-        api(value,page).then(({total,hits} ) => {
-            
-            if(total === 0){
-                setCards([])
-                setIsLoading(false)
-                setNoData(true)
-                setButton(false)
-                return;
-            }
-            setIsLoading(true)
-            setCards(hits)
-              setIsLoading(false)
-              setNoData(false)
-              setButton(true)
-        }).catch((error) => console.log(error))
-    },[value])
-
-        
+const ImageGallery = ({values}) => {
         return (
             <div>
                 <ul className={css.ImageGallery}>
-                {isLoading && <Audio
-            height="80"
-            width="80"
-            radius="9"
-            color="green"
-            ariaLabel="loading"
-            wrapperStyle
-            wrapperClass
-          />}
-                {noData && (<h1>No data found</h1>)}
-            {cards && (cards.map(({id,tags,webformatURL,largeImageURL}) => (
+            {values.map(({id,tags,webformatURL,largeImageURL}) => (
                 <ImageGalleryItem 
                 key={id}
                 alt={tags}
                 img={webformatURL}
                 largeImage={largeImageURL}
                 ></ImageGalleryItem>
-            ))) }
+            ))}
         </ul>
-        {button && <Button click={onClick}/>}
         </div>
             )
     }
